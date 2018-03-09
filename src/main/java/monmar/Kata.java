c1cb72b26dd63639f39a1bf177ec7a52c05099ab
Marcin Pastwa <marcinpastwa@wp.pl>
2018-03-09 20:53:12 +0100

    ex.scramble

--- src/main/java/monmar/Kata.java
+++ src/main/java/monmar/Kata.java
@@ -1,6 +1,8 @@
 package monmar;
 
+import java.lang.reflect.Array;
 import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Kata {
@@ -123,4 +125,88 @@ public class Kata {
 //        return vowelsCount-str.length();
     }
 
+    public static int digital_root(int n) {
+        return (n != 0 && n%9 == 0) ? 9 : n % 9;
+    }
+
+    public static String createPhoneNumber(int[] numbers) {
+//        StringBuilder sb = new StringBuilder();
+//            return sb.append("(").append(numbers[0]).append(numbers[1]).append(numbers[2]).append(")")
+//                    .append(" ")
+//                    .append(numbers[3])
+//                    .append(numbers[4])
+//                    .append(numbers[5])
+//                    .append("-")
+//                    .append(numbers[6])
+//                    .append(numbers[7])
+//                    .append(numbers[8])
+//                    .append(numbers[9]).toString();
+
+        return String.format("(%d%d%d) %d%d%d-%d%d%d%d",numbers[0],numbers[1],numbers[2],numbers[3],numbers[4],numbers[5],numbers[6],numbers[7],numbers[8],numbers[9]);
+
+        }
+
+    public String toJadenCase(String phrase) {
+
+        if (null == phrase || phrase.length() == 0) {
+            return null;
+        }
+        return Arrays.stream(phrase.split(" "))
+                .map(i -> i.substring(0, 1).toUpperCase() + i.substring(1, i.length()))
+                .collect(Collectors.joining(" "));
+    }
+
+    public static long digPow(int n, int p) {
+        String intString = String.valueOf(n);
+        long sum = 0;
+        for (int i = 0; i < intString.length(); ++i, ++p)
+            sum += Math.pow(Character.getNumericValue(intString.charAt(i)), p);
+        return (sum % n == 0) ? sum / n : -1;
+    }
+
+    public static int duplicateCount(String text) {
+
+        Map<Character,Integer> duplicateMap = new HashMap<>();
+        for (int i=0;i<text.length();i++){
+            duplicateMap.put(text.charAt(i),duplicateMap.containsKey(text.charAt(i)) ? duplicateMap.get(text.charAt(i))+1 : 1);
+        }
+        return (int) duplicateMap.values().stream().filter(c -> c > 1).count();
+    }
+
+    public static String changeWeightInRighOrder(String s) {
+
+        List<String> list = Stream.of(s.split(" ")).collect(Collectors.toList());
+        Map<String,Integer> weightMap = new TreeMap<>();
+        int sum=0;
+        for (int i=0;i<list.size();i++){
+//            weightMap.putIfAbsent(list.get(i),list.get(i).chars().sum());
+            char[] chars = list.get(i).toCharArray();
+            for (int k=0;k<chars.length;k++){
+                sum+=Character.getNumericValue(chars[k]);
+            }
+            weightMap.putIfAbsent(list.get(i),sum);
+        }
+//        return weightMap.entrySet().stream().sorted(Map.Entry.<String, Integer>comparingByValue()).collect(Collectors.toList());
+        return "sd";
+    }
+
+    public static String[] inArray(String[] array1, String[] array2) {
+
+//        Arrays.stream(array1).
+
+        return new String[] { "arp" };
+    }
+
+    public static boolean scramble(String str1, String str2) {
+        char[] str2Array = str2.toCharArray();
+        List<Character> chars = str1.chars().mapToObj(e->(char)e).collect(Collectors.toList());
+        int i =0;
+        for (Character c : str2Array) {
+            if (chars.contains(c)) {
+                i++;
+                chars.remove(c);
+            }
+        }
+        return i==str2.length() ? true : false;
+    }
 }
--- src/test/java/monmar/KataTest.java
+++ src/test/java/monmar/KataTest.java
@@ -2,7 +2,11 @@ package monmar;
 
 import org.junit.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class KataTest {
     Kata kata = new Kata();
@@ -89,5 +93,89 @@ public class KataTest {
     @Test
     public void testCase2() {
         assertEquals("Nope!", 11, kata.getCount("abra   cadabra  aafsdeeee"));
+    }
+
+
+        @Test
+        public void Tests() {
+            assertEquals( "Nope!" , kata.digital_root(146), 2);
+        }
+
+    @Test
+    public void tests() {
+        assertEquals("(123) 456-7890", Kata.createPhoneNumber(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 0}));
+    }
+
+    @Test
+    public void testJaden() {
+        assertEquals("toJadenCase doesn't return a valide JadenCase String! try again please :)", kata.toJadenCase("most trees are blue"), "Most Trees Are Blue");
+    }
+
+    @Test
+    public void testNullArg() {
+        assertNull("Must return null when the arg is null", kata.toJadenCase(null));
+    }
+
+    @Test
+    public void testEmptyArg() {
+        assertNull("Must return null when the arg is null", kata.toJadenCase(""));
+    }
+    @Test
+    public void TestDigit1() {
+        assertEquals(1, kata.digPow(89, 1));
+    }
+    @Test
+    public void TestDigit2() {
+        assertEquals(-1, kata.digPow(92, 1));
+    }
+    @Test
+    public void TestDigit3() {
+        assertEquals(51, kata.digPow(46288, 3));
+    }
+
+    @Test
+    public void abcdeReturnsZero() {
+        assertEquals(0, kata.duplicateCount("abcde"));
+    }
+
+    @Test
+    public void abcdeaReturnsOne() {
+        assertEquals(1, kata.duplicateCount("abcdea"));
+    }
+
+    @Test
+    public void indivisibilityReturnsOne() {
+        assertEquals(1, kata.duplicateCount("indivisibility"));
+    }
+
+    @Test
+    public void changeWeightInRighOrderTest(){
+        assertThat(kata.changeWeightInRighOrder("56 65 74 100 99 68 86 180 90"), is("100 180 90 56 65 74 68 86 99"));
+    }
+    @Test
+    public void test1() {
+        String a[] = new String[]{ "arp", "live", "strong" };
+        String b[] = new String[] { "lively", "alive", "harp", "sharp", "armstrong" };
+        String r[] = new String[] { "arp", "live", "strong" };
+        assertArrayEquals(r, Kata.inArray(a, b));
+    }
+
+    private static void testing(boolean actual, boolean expected) {
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void checkIfWordIsInString() {
+        System.out.println("Fixed Tests scramble");
+        testing(Kata.scramble("rkqodlw","world"), true);
+        testing(Kata.scramble("cedewaraaossoqqyt","codewars"),true);
+        testing(Kata.scramble("katas","steak"),false);
+        testing(Kata.scramble("scriptjavx","javascript"),false);
+        testing(Kata.scramble("scriptingjava","javascript"),true);
+        testing(Kata.scramble("scriptsjava","javascripts"),true);
+        testing(Kata.scramble("javscripts","javascript"),false);
+        testing(Kata.scramble("aabbcamaomsccdd","commas"),true);
+        testing(Kata.scramble("commas","commas"),true);
+        testing(Kata.scramble("sammoc","commas"),true);
     }
 }
