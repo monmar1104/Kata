f0bdb3a3472daeb3b4b3486401214d5ac6099e76
Marcin Pastwa <marcinpastwa@wp.pl>
2018-05-18 18:25:58 +0200

    IntStram Solution

---
+++ src/main/java/monmar/Solution.java
@@ -0,0 +1,9 @@
+package monmar;
+
+import java.util.stream.IntStream;
+
+public class Solution {
+    public int solution(int number) {
+        return IntStream.range(0,number).filter(i -> i % 3 == 0 || i % 5 ==0).sum();
+    }
+}
---
+++ src/test/java/monmar/SolutionTest.java
@@ -0,0 +1,12 @@
+package monmar;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+    @Test
+    public void test() {
+        assertEquals(23, new Solution().solution(10));
+    }
+
+}
