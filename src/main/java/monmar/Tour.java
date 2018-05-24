491b161c7528768b3a5d182ffdc87902d55bc877
Marcin Pastwa <marcinpastwa@wp.pl>
2018-05-24 18:53:21 +0200

    Tour start

---
+++ src/main/java/monmar/Tour.java
@@ -0,0 +1,10 @@
+package monmar;
+
+import java.util.Map;
+
+public class Tour {
+    public static int tour(String[] arrFriends, String[][] ftwns, Map<String, Double> h) {
+        // your code
+        return 1;
+    }
+}
