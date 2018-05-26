55c40e7899f0927c71878978af9d7785528fb8e7
Marcin Pastwa <marcinpastwa@wp.pl>
2018-05-26 17:30:51 +0200

    StreetFighter start

---
+++ src/main/java/monmar/StreetFighter.java
@@ -0,0 +1,11 @@
+package monmar;
+
+public class StreetFighter {
+    public static String[] superStreetFighterize(String[][] fighters, int[] position, String[] moves) {
+        String[] selectedFighters = new String[moves.length];
+
+
+
+        return null;
+    }
+}
